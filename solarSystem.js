b2ae37f279675b129a3f2ecf064dcffd92807551
Facundo Castro <4qsmkrdtp6@privaterelay.appleid.com>
2026-04-27 18:07:39 -0400

    Delete solarSystem.js

--- solarSystem.js
+++
@@ -1,5 +0,0 @@
-export const solarSystem = [
-  { name:"Sun", mass:1, x:0, y:0, vx:0, vy:0 },
-  { name:"Earth", mass:3.003e-6, x:1, y:0, vx:0, vy:6.28 },
-  { name:"Mars", mass:3.213e-7, x:1.52, y:0, vx:0, vy:5.08 }
-];
